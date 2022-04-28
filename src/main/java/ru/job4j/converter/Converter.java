d53ce232aaf9316265808b220cc587bc8a285c10
Neonod <neonod404@yandex.ru>
2022-04-28 08:29:14 +0300

    5. Конвертер валюты [#41583 #7873]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -15,6 +15,5 @@ public class Converter {
         float dollar = Converter.rubleToDollar(200);
         System.out.println("140 rubles are " + euro + " euro.");
         System.out.println("200 rubles are " + dollar + " dollar");
-
     }
 }
