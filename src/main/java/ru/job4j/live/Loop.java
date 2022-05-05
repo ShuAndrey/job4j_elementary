a284ea057e91a8fe20c4974bd40c72706ad6083f
Andrey Shulgin <neonod404@yandex.ru>
2022-05-05 10:20:40 +0300

    5.9.1. Генерация конструкций в IDEA [#279134]

---
+++ src/main/java/ru/job4j/live/Loop.java
@@ -0,0 +1,10 @@
+package ru.job4j.live;
+
+public class Loop {
+
+    public static void main(String[] args) {
+        for (int index = 0; index <= 10; index++) {
+            System.out.println(index);
+        }
+    }
+}
