005e12ffe1c4c9ef09670d7fd0f710b3ad1c6795
Andrey Shulgin <neonod404@yandex.ru>
2022-05-18 17:30:11 +0300

    3. Перегрузить метод max для трех чисел [#122644]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -1,8 +1,15 @@
 package ru.job4j.condition;
 
 public class Max {
-    public static int max(int left, int right) {
-        int result = left > right ? left : right;
-        return result;
+    public static int max(int a, int b) {
+        return a > b ? a : b;
+    }
+
+    public static int max(int a, int b, int c) {
+        return a > max(b, c) ? a : max(b, c);
+    }
+
+    public static int max(int a, int b, int c, int d) {
+        return max(max(a, b), max(c, d));
     }
 }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -6,28 +6,36 @@ import org.junit.Test;
 public class MaxTest {
     @Test
     public void whenMax1To2Then2() {
-        int left = 1;
-        int right = 2;
-        int result = Max.max(left, right);
+        int result = Max.max(1, 2);
         int expected = 2;
         Assert.assertEquals(result, expected);
     }
 
     @Test
     public void whenMax2To1Then1() {
-        int left = 2;
-        int right = 1;
-        int result = Max.max(left, right);
+        int result = Max.max(2, 1);
         int expected = 2;
         Assert.assertEquals(result, expected);
     }
 
     @Test
     public void whenMax2To2Then2() {
-        int left = 2;
-        int right = 2;
-        int result = Max.max(left, right);
+        int result = Max.max(2, 2);
         int expected = 2;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax1And2And3Then3() {
+        int result = Max.max(1, 2, 3);
+        int expected = 3;
+        Assert.assertEquals(result, expected);
+    }
+
+    @Test
+    public void whenMax3And1And8And4Then8() {
+        int result = Max.max(3, 1, 8, 4);
+        int expected = 8;
         Assert.assertEquals(result, expected);
     }
 }
