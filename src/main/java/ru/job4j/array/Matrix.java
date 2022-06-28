1e8f78cb5937514b1e8cb051b217f458ed2454b9
Andrey Shulgin <neonod404@yandex.ru>
2022-06-28 23:03:21 +0300

    0.1. FileOutputStream. [#252488]

--- src/main/java/ru/job4j/array/Matrix.java
+++ src/main/java/ru/job4j/array/Matrix.java
@@ -1,13 +1,22 @@
 package ru.job4j.array;
 
+import java.io.FileOutputStream;
+
 public class Matrix {
 
     public static int[][] multiple(int size) {
         int[][] rst = new int[size][size];
-        for (int row = 0; row < rst.length; row++) {
-            for (int cell = 0; cell < rst[row].length; cell++) {
-                rst[row][cell] = (row + 1) * (cell + 1);
+        try (FileOutputStream out = new FileOutputStream("result.txt")) {
+            for (int row = 0; row < rst.length; row++) {
+                for (int cell = 0; cell < rst[row].length; cell++) {
+                    rst[row][cell] = (row + 1) * (cell + 1);
+                    out.write(Integer.toString(rst[row][cell]).getBytes());
+                    out.write(" ".getBytes());
+                }
+                out.write(System.lineSeparator().getBytes());
             }
+        } catch (Exception e) {
+            e.printStackTrace();
         }
         return rst;
     }
