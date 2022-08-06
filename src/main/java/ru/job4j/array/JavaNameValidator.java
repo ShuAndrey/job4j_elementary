de22455f0f5569273156610c54432cae0b8ecfbf
Andrey Shulgin <neonod404@yandex.ru>
2022-08-06 07:37:36 +0300

    6.9. String - это массив [#504884]

---
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -0,0 +1,36 @@
+package ru.job4j.array;
+
+/**
+ * @author Andrey Shulgin
+ */
+public class JavaNameValidator {
+    public static boolean isNameValid(String name) {
+        boolean result = !name.isEmpty() && isLowerLatinLetter(name.codePointAt(0));
+        if (result) {
+            int code;
+            for (int index = 1; index < name.length(); index++) {
+                code = name.codePointAt(index);
+                if (!isSpecialSymbol(code)
+                        && !isUpperLatinLetter(code)
+                        && !isLowerLatinLetter(code)
+                        && !Character.isDigit(code)) {
+                    result = false;
+                    break;
+                }
+            }
+        }
+        return result;
+    }
+
+    public static boolean isSpecialSymbol(int code) {
+        return code == 36 || code == 95;
+    }
+
+    public static boolean isUpperLatinLetter(int code) {
+        return code >= 65 && code <= 90;
+    }
+
+    public static boolean isLowerLatinLetter(int code) {
+        return code >= 97 && code <= 122;
+    }
+}
